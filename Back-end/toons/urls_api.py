9241e1d953b94de0c17ded3ae08ea7eee24de4de
daeil <il97testcode@gmail.com>
2025-12-14 11:30:53 +0900

    (dev) 추천 기능 추가

--- Back-end/toons/urls_api.py
+++ Back-end/toons/urls_api.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .api_views import webtoon_list, webtoon_detail, toggle_favorite, my_favorites
-
+from .api_views import (webtoon_list,
+                        webtoon_detail,
+                        toggle_favorite,
+                        my_favorites,
+                        recommend_by_favorites,
+                        recommend_by_synopsis,
+                        )
 urlpatterns = [
     path('webtoons/', webtoon_list, name='api-webtoon-list'),
     path('webtoons/<int:webtoon_id>/', webtoon_detail, name='api-webtoon-detail'),
     path('webtoons/<int:webtoon_id>/favorite/', toggle_favorite, name='api-toggle-favorite'),
     path('me/favorites/', my_favorites, name='api-my-favorites'),
+    # 추천 시스템 URL 추가
+    path('webtoons/favorites/', recommend_by_favorites, name='api-recommend-favorites'),
+    path('webtoons/synopsis/<int:webtoon_id>/', recommend_by_synopsis, name='api-recommend-synopsis'),
 ]
